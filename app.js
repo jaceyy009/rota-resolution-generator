c0cb1a531a7b9a4ead279570a3328a574f3c83d9
jaceyy009 <120229247+jaceyy009@users.noreply.github.com>
2026-01-07 20:23:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 document.getElementById("resolutionForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const title = document.getElementById("title").value;
-  const background = document.getElementById("background").value;
-  const action = document.getElementById("action").value;
+  const data = {
+    resolution_number: document.getElementById("resolutionNumber").value,
+    title: document.getElementById("title").value,
+    whereas_1: document.getElementById("whereas1").value,
+    whereas_2: document.getElementById("whereas2").value,
+    whereas_3: document.getElementById("whereas3").value,
+    section_1: document.getElementById("section1").value,
+    section_2: document.getElementById("section2").value
+  };
 
-  const draft = `
-  READING & OUTSKIRTS TRANSPORTATION AUTHORITY (ROTA)
+  let output = `
+READING & OUTSKIRTS TRANSPORTATION AUTHORITY
 
-  RESOLUTION
+RESOLUTION NO. ${data.resolution_number}
 
-  ${title}
+${data.title}
 
-  WHEREAS, ${background};
+WHEREAS, ${data.whereas_1}; and
+WHEREAS, ${data.whereas_2}; and
+WHEREAS, ${data.whereas_3}.
 
-  NOW, THEREFORE, BE IT RESOLVED THAT:
+NOW, THEREFORE, BE IT RESOLVED THAT:
 
-  ${action}
-    `;
+SECTION 1. ${data.section_1}
+SECTION 2. ${data.section_2}
+`;
 
-  document.getElementById("output").textContent = draft;
+  document.getElementById("output").textContent = output;
 });
